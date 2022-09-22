ad71eb6ba63dd710e4eeffc8b7743ed483ba2165
cleverplant <cleverplant39@gmail.com>
2022-09-22 07:33:07 +0200

    опять 25

---
+++ корзина/2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Console.Write("введите число для сравнения: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+int number =10;
+test1(number);
+int test1(int n = 0)
+{
+    if (n == 10)
+        return 1;
+    else if (n == 11)
+        return 2;
+    else 
+        return 3;    
+}
+Console.Write(test1());
+
+// int number =10;
+// test(number);
+// int test(int n = 1)
+// {
+//     if (n >= 10)
+//         return 0;
+//     else if (n < 10)
+//         return n;
+//     else
+//         return 100;
+// }
+// Console.Write(test());
+
+
+
