c1b2c28c31e76747ea381f42f73f8898aa635d43
cleverplant <cleverplant39@gmail.com>
2022-09-22 14:57:42 +0200

    добавил шпаргалку по форме функции и массива

---
+++ шпаргалка по функциям и массивам/Program_шпора.cs
@@ -0,0 +1,24 @@
+﻿/* 
+ВозвращаемыйТипДанных ИмяМетода([ТипДанных1 ИмяАргумента1, ... ])
+|          int       |   Max   |    (int   |      arg1,   |   int arg2, | int arg3)                                              */
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+ //              0    1   2  
+int[] array = { 11, 211, 31};
+
+int max = Max(array[0], array[1], array[2]);
+Console.WriteLine(Max(array[0], array[1], array[2]));
+Console.WriteLine(max);
+/*
+ВозвращаемыйТипДанных ИмяМетода([ТипДанных1 ИмяАргумента1, ... ])
+|          int       |   Max   |    (int   |      arg1,   |   int arg2, | int arg3)
+{
+Тело Метода
+return ЗначениеСоответствующееВозвращаемомуТипуДанных;
+}
+*/
