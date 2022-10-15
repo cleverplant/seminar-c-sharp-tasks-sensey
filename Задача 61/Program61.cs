6bb30a910e696a516ec41bcd56763d032fe55cd4
cleverplant <cleverplant39@gmail.com>
2022-10-15 13:02:51 +0200

    seminar 9 run

--- Задача 61/Program61.cs
+++ Задача 61/Program61.cs
@@ -2,6 +2,9 @@
 Дополнительная задача:
 Задача 61:Задайте две матрицы.
 Напишите программу, которая будет находить произведение двух матриц.
+
+                   СДЕЛАЛ В ДОМАШКЕ
+
 */
 
 void z61()
@@ -13,33 +16,33 @@ Random random = new Random();
     Console.WriteLine($"Строк:{rows}, Столбцов:{columns}");
 
 
-    FillArray(numbers);
-    PrintArray(numbers);
+//     FillArray(numbers);
+//     PrintArray(numbers);
 
-void FillArray(int[,] matr)
-    {
-        for (int i = 0; i < matr.GetLength(0); i += 2)
-        {
-            for (int j = 0; j < matr.GetLength(1); j += 2)
-            {
-                matr[i, j] = new Random().Next(1, 10);// 
-            }
-            //Console.WriteLine();
-        }
-        Console.WriteLine();
-    }
+// void FillArray(int[,] matr)
+//     {
+//         for (int i = 0; i < matr.GetLength(0); i += 2)
+//         {
+//             for (int j = 0; j < matr.GetLength(1); j += 2)
+//             {
+//                 matr[i, j] = new Random().Next(1, 10);// 
+//             }
+//             //Console.WriteLine();
+//         }
+//         Console.WriteLine();
+//     }
 
-    void PrintArray(int[,] matr)
-    {
-        for (int i = 0; i < matr.GetLength(0); i++)// rows - строки
-        {
-            for (int j = 0; j < matr.GetLength(1); j++)// columns -столбцы
-            {
-                Console.Write($"{matr[i, j] + " "}");// 
-            }
-            Console.WriteLine();
-        }
-    }
+//     void PrintArray(int[,] matr)
+//     {
+//         for (int i = 0; i < matr.GetLength(0); i++)// rows - строки
+//         {
+//             for (int j = 0; j < matr.GetLength(1); j++)// columns -столбцы
+//             {
+//                 Console.Write($"{matr[i, j] + " "}");// 
+//             }
+//             Console.WriteLine();
+//         }
+//     }
 
 
 
---
+++ Задача 63/Program63.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 63:Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void cycle()
+{
+
+
+
+    
+    FillArray(numbers);
+    PrintArray(numbers);
+
+void FillArray(int[,] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i += 2)
+        {
+            for (int j = 0; j < matr.GetLength(1); j += 2)
+            {
+                matr[i, j] = new Random().Next(1, 10);// 
+            }
+            //Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    void PrintArray(int[,] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)// rows - строки
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)// columns -столбцы
+            {
+                Console.Write($"{matr[i, j] + " "}");// 
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+
+
+}
+cycle();
+
+
+void recursya()
+{
+
+
+
+
+}
+recursya();
